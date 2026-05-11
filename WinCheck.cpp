52d9d03ae557e65ceae625473da9b1753f6f2824
jghdfrsth-rgb <jghdfrsth@gmail.com>
2026-05-11 23:24:01 +0300

    Update WinCheck.cpp

--- WinCheck.cpp
+++ WinCheck.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool checkWin(char player) {
     // Check horizontal
     for (int r = 0; r < 7; r++) {
-        for (int c = 0; c <= 3; c++) {
+        for (int c = 0; c <=3; c++) {
             if (board[r][c] == player && 
                 board[r][c + 1] == player && 
                 board[r][c + 2] == player && 
@@ -14,7 +14,7 @@ bool checkWin(char player) {
         }
     }
     // Check vertical
-    for (int r = 0; r < 2; r++) {
+    for (int r = 0; r <= 3; r++) {
         for (int c = 0; c < 7; c++) {
             if (board[r][c] == player && 
                 board[r + 1][c] == player && 
